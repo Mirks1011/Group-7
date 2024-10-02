1652514144b877edd03c38cd7624345e87cd04b9
Mirks1011 <laride.kurtjoel.avenido@gmail.com>
2024-10-02 18:04:59 +0800

    Add files via upload

--- JS/script.js
+++ JS/script.js
@@ -4,7 +4,11 @@ const body = document.querySelector("body"),
     searchBtn = document.querySelector(".search-box"),
     modeSwitch = document.querySelector(".toggle-switch"),
     modeText = document.querySelector(".mode-text");
-
+/* DARK MODE BUTTON SCRIPT */
     modeSwitch.addEventListener("click", () =>{
         body.classList.toggle("dark");
+    })
+
+    toggle.addEventListener("click", () =>{
+        sidebar.classList.toggle("close");
     })
